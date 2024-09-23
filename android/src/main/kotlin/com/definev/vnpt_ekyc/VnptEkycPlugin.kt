d35e2cae5c9ad62d7a6394e53196078ffd1422c0
definev <daiduong.workmail@gmail.com>
2024-09-23 10:54:51 +0700

    Refactor VnptEkycPlugin to handle NFC result and update return values

--- android/src/main/kotlin/com/definev/vnpt_ekyc/VnptEkycPlugin.kt
+++ android/src/main/kotlin/com/definev/vnpt_ekyc/VnptEkycPlugin.kt
@@ -12,41 +12,38 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
 import java.util.Locale
 
 
 /** VnptEkycPlugin */
 class VnptEkycPlugin : FlutterPlugin, ActivityAware, ActivityResultListener, VnptEkycPigeon {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
-    private lateinit var channel: MethodChannel
     var activity: Activity? = null
 
-    var ekycCompletionCallback: ((Result<Map<String, String?>>) -> Unit)? = null
+    private var ekycCompletionCallback: ((Result<Map<String, String?>>) -> Unit)? = null
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         VnptEkycPigeon.setUp(flutterPluginBinding.binaryMessenger, this)
     }
 
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        VnptEkycPigeon.setUp(binding.binaryMessenger, null)
+        activity = null
+        ekycCompletionCallback = null
+    }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
         activity = binding.activity
-    }
-
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        VnptEkycPigeon.setUp(binding.binaryMessenger, null)
-    }
-
-    override fun onDetachedFromActivityForConfigChanges() {
-        activity = null
+        binding.addActivityResultListener(this)
     }
 
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
         activity = binding.activity
+        binding.addActivityResultListener(this)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
     }
 
     override fun onDetachedFromActivity() {
@@ -103,26 +100,28 @@ class VnptEkycPlugin : FlutterPlugin, ActivityAware, ActivityResultListener, Vnp
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
         val result: Result<Map<String, String?>>?
         if (requestCode == 1 && resultCode == FlutterFragmentActivity.RESULT_OK) {
-            val personalInformation = data?.getStringExtra(KeyResultConstantsNFC.PERSONAL_INFORMATION)
-//            val avatarPath = data?.getStringExtra(KeyResultConstantsNFC.IMAGE_AVATAR_CARD_NFC)
+            val qrCode = data?.getStringExtra(KeyResultConstantsNFC.QR_CODE_RESULT_NFC)
+            val avatar = data?.getStringExtra(KeyResultConstantsNFC.IMAGE_AVATAR_CARD_NFC)
             val logNFC = data?.getStringExtra(KeyResultConstantsNFC.LOG_NFC)
 
-            println("personalInformation: $personalInformation")
+            println("personalInformation: $qrCode")
             println("logNFC: $logNFC")
 
-            if (personalInformation == null || logNFC == null) {
+            if (qrCode == null || logNFC == null || avatar == null) {
                 result = Result.success(
                     mapOf(
-                        "personal_information" to null,
+                        "qr_code" to null,
                         "log_nfc" to null,
+                        "avatar" to null,
                         "error" to "cancel by user",
                     )
                 )
             } else {
                 result = Result.success(
                     mapOf(
-                        "personal_information" to personalInformation,
+                        "qr_code" to qrCode,
                         "log_nfc" to logNFC,
+                        "avatar" to avatar,
                     )
                 )
             }
